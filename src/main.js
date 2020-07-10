81af6c13fd057b3d441d62f17ab1a28867324153
polunwu <polunwu@gmail.com>
2020-07-10 16:11:30 +0800

    Add auto login from localstaorage data, add simple security measure feature

--- src/main.js
+++ src/main.js
@@ -2,11 +2,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './vuex/store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  created() {
+    const userString = localStorage.getItem('user')
+    if (userString) {
+      const userData = JSON.parse(userString)
+      this.$store.commit('SET_USER_DATA', userData)
+    }
+    // If someone attempts to use a fake token in localStorage
+    // They will be logged out as soon as they try to access private resources.
+    axios.interceptors.response.use(
+      response => response,
+      error => {
+        if (error.response.status === 401) {
+          this.$store.dispatch('logout')
+        }
+        return Promise.reject(error)
+      }
+    )
+  },
   render: h => h(App)
 }).$mount('#app')
